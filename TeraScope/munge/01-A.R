76b0ee97311c7cd7b9d9c46095a1bc105b6849b4
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-17 01:26:15 +0000

    Initial preprocessing script

--- TeraScope/munge/01-A.R
+++ TeraScope/munge/01-A.R
@@ -1,1 +1,14 @@
-# Example preprocessing script.
+# Preprocessing script 1
+## Reading the file from the data folder
+tasks_data = task.x.y
+gpu_data = gpu
+checkpoints_data = application.checkpoints
+
+## Removing duplicate records
+checkpoints_data = checkpoints_data %>% distinct()
+gpu_data = gpu_data %>% distinct()
+
+## Combine the datasets and order based on hostname and timestamp variables
+checkpoints_tasks_data = merge(x = checkpoints_data, y = tasks_data, by = "taskId", all = TRUE)
+tera_data = full_join(checkpoints_tasks_data,gpu_data,by = c("timestamp", "hostname"))
+tera_data = tera_data[order(tera_data$hostname, tera_data$timestamp),]
