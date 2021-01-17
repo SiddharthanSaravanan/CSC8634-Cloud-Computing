e32cf1b30225ba999cfb5d23ecd79d61752ae5e1
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-17 03:33:12 +0000

    Computing host gpu performance dataset

--- TeraScope/munge/02-A.R
+++ TeraScope/munge/02-A.R
@@ -22,3 +22,24 @@ master_tera_data = full_join(final_pivot_tera_data, final_task_gpu_performance, 
 
 gpu_total_render = master_tera_data %>% filter(eventName == "TotalRender" & eventType == "STOP")
 gpu_total_render = gpu_total_render[,c(2,3,7,8,10,6,19,20,21,22,23)]
+
+
+#----------------------------------------------------------------------------------
+## gpu performance for each hostname
+
+host_gpu_performance = final_pivot_tera_data[,c(3,13,14,15,16)]
+host_gpu_performance = na.omit(host_gpu_performance)
+rownames(host_gpu_performance) <- 1:nrow(host_gpu_performance)
+host_gpu_performance = host_gpu_performance %>% filter(gpuUtilPerc>10 & gpuMemUtilPerc>10)
+
+
+
+## final_host_gpu_performace = generating by summarizing the values
+
+final_host_gpu_performance = host_gpu_performance
+final_host_gpu_performance = final_host_gpu_performance %>% 
+  group_by(final_host_gpu_performance$hostname) %>%   summarise(total_power_drawnW = sum(powerDrawWatt), average_gpu_tempC = mean(gpuTempC),
+                                                                average_gpu_utilP = mean(gpuUtilPerc),average_gpu_memory_utilP = mean(gpuMemUtilPerc))
+
+
+names(final_host_gpu_performance)[1] = "hostname"
