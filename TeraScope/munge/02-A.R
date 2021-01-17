7a8d2f64107d200ca4127fd605a7d11a2d1fe329
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-17 03:27:50 +0000

    Preprocessing script 2

---
+++ TeraScope/munge/02-A.R
@@ -0,0 +1,24 @@
+# gpu performance preprocessing step
+
+task_gpu_performance = final_pivot_tera_data[,c(2,13,14,15,16)]
+task_gpu_performance = na.omit(task_gpu_performance)
+rownames(task_gpu_performance) <- 1:nrow(task_gpu_performance)
+task_gpu_performance = task_gpu_performance %>% filter(gpuUtilPerc>10 & gpuMemUtilPerc>10)
+
+
+
+## final_task_gpu_performace = generating by summarizing the values
+
+final_task_gpu_performance = task_gpu_performance
+final_task_gpu_performance = final_task_gpu_performance %>% 
+  group_by(final_task_gpu_performance$taskId) %>%   summarise(total_power_drawnW = sum(powerDrawWatt), average_gpu_tempC = mean(gpuTempC),
+                                                         average_gpu_utilP = mean(gpuUtilPerc),average_gpu_memory_utilP = mean(gpuMemUtilPerc))
+names(final_task_gpu_performance)[1] = "taskId"
+
+
+master_tera_data = full_join(final_pivot_tera_data, final_task_gpu_performance, by = c("taskId"))
+
+## GPU performance data based on Total Render
+
+gpu_total_render = master_tera_data %>% filter(eventName == "TotalRender" & eventType == "STOP")
+gpu_total_render = gpu_total_render[,c(2,3,7,8,10,6,19,20,21,22,23)]
