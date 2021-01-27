bfbb66b88eeffcfd9a94976347a8928785cc709a
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-27 20:06:16 +0000

    Analysis file

---
+++ TeraScope/src/Analysis_4.R
@@ -0,0 +1,25 @@
+#Finding top 10 task id's based on GPU variables:
+outliers_total_power_drawn_top10 = as.data.frame(tail(outliers_total_power_drawn[order(outliers_total_power_drawn$total_power_drawnW),],10))
+
+outliers_average_gpu_temp_top10 = as.data.frame(tail(outliers_average_gpu_temp[order(outliers_average_gpu_temp$average_gpu_tempC),],10))
+
+outliers_average_gpu_util_top10 = as.data.frame(tail(outliers_average_gpu_util[order(outliers_average_gpu_util$average_gpu_utilP),],10))
+
+outliers_average_gpu_memory_util_top10 = as.data.frame(tail(outliers_average_gpu_memory_util[order(outliers_average_gpu_memory_util$average_gpu_memory_utilP),],10))
+
+outliers_total_power_drawn_top10$category = "Total_power_drawn"
+outliers_average_gpu_temp_top10$category = "Average_GPU_Temp"
+outliers_average_gpu_util_top10$category = "Average_GPU_Util"
+outliers_average_gpu_memory_util_top10$category = "Average_GPU_Memory_Util"
+
+top_10_outliers_gpu = rbind(outliers_total_power_drawn_top10, outliers_average_gpu_temp_top10, outliers_average_gpu_util_top10,
+                                outliers_average_gpu_memory_util_top10)
+
+
+
+#Finding top 10 Hostnames based on GPU variables:
+
+outliers_gpu_total_power_top10 = as.data.frame(tail(outliers_final_host_gpu_performance_1[order(outliers_final_host_gpu_performance_1$total_power_drawnW),],10))
+outliers_gpu_gpu_temp_top10 = as.data.frame(tail(outliers_final_host_gpu_performance_2[order(outliers_final_host_gpu_performance_2$average_gpu_tempC),],10))
+outliers_gpu_gpu_util_top10 = as.data.frame(tail(outliers_final_host_gpu_performance_3[order(outliers_final_host_gpu_performance_3$average_gpu_utilP),],10))
+outliers_gpu_gpu_memory_util_top10 = as.data.frame(tail(outliers_final_host_gpu_performance_4[order(outliers_final_host_gpu_performance_4$average_gpu_memory_utilP),],10))
