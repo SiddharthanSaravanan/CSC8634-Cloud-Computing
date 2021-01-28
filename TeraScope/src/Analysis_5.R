b92a0a11030d16da94c8fb44fb1117722ca61780
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-28 11:05:27 +0000

    Key image GPU change analysis file

---
+++ TeraScope/src/Analysis_5.R
@@ -0,0 +1,9 @@
+#Key image analysis of Poor performing GPU's:
+
+
+(plot_19 = ggplot(gpu_total_render, aes(y, -x)) +geom_tile(aes(fill = average_gpu_memory_utilP))+ 
+  geom_point(data = outliers_total_power_drawn,aes(y,-x, size = total_power_drawnW)) +
+  geom_point(data = poor_performance_gpu,aes(y,-x, color = hostname)))
+
+
+ggsave(file.path('graphs', 'Power drawn by gpu nodes in series.png'))
