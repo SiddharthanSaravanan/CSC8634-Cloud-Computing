320e40acf564114c81923ceefbfbf466d0872fd6
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2021-01-28 18:02:50 +0000

    Graphical Analysis file 5

--- TeraScope/src/Analysis_5.R
+++ TeraScope/src/Analysis_5.R
@@ -3,7 +3,8 @@
 
 (plot_19 = ggplot(gpu_total_render, aes(y, -x)) +geom_tile(aes(fill = average_gpu_memory_utilP))+ 
   geom_point(data = outliers_total_power_drawn,aes(y,-x, size = total_power_drawnW)) +
-  geom_point(data = poor_performance_gpu,aes(y,-x, color = hostname)))
+  geom_point(data = poor_performance_gpu,aes(y,-x, color = hostname))+ labs(title = "Outliers of Poor performance GPU hostnames",
+                                                                            x = "Y co-ordinate", y = "-X co-ordinate"))
 
 
 ggsave(file.path('graphs', 'GPU nodes with poor performance.png'))
